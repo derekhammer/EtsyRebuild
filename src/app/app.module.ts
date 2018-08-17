70c4da8fe53924774cbc291710593b77c692b377
Derek Hammer <derekhammer@hotmail.com>
2018-08-17 13:33:43 -0700

    add slideshow that works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SlideShowComponent } from './slide-show/slide-show.component';
+import { CatagoryComponent } from './catagory/catagory.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    SlideShowComponent
+    SlideShowComponent,
+    CatagoryComponent
   ],
   imports: [
     BrowserModule
