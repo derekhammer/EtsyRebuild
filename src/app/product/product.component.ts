f9de35e1a655d24f39c5254ebf28e7c71763b4b4
Derek Hammer <derekhammer@hotmail.com>
2018-08-24 12:10:26 -0700

    tries to make new carousel work

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,10 +8,10 @@ import { Product } from '../models/product.model'
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [
-    new Product("Hand carved soapstone bears", "Elliot", 4.5, 4.99, "placeholder"),
-    new Product("Customized designer glasses", "Hisato", 4.8, 49.99, "placeholder"),
-    new Product("Leftovers", "James", 0.3, 0.50, "placeholder"),
-    new Product("EZ Sleep Pillow", "Nick", 4.2, 39.99, "placeholder")
+    new Product("Hand carved soapstone bears", "Elliot", 4.5, 4.99, "./assets/bears.jpg"),
+    new Product("Customized designer glasses", "Hisato", 4.8, 49.99, "./assets/bears.jpg"),
+    new Product("Leftovers", "James", 0.3, 0.50, "./assets/bears.jpg"),
+    new Product("EZ Sleep Pillow", "Nick", 4.2, 39.99, "./assets/bears.jpg")
   ];
 
 
