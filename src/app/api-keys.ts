e32c3a007b4c2d40320f1aeedc136ca1b312d5bb
Derek Hammer <derekhammer@hotmail.com>
2018-08-31 03:41:00 -0700

    small styling

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyATPTmdb5U3iPKE_bueEKEeDfBTiOAr8SQ",
+  authDomain: "etsy-93f9a.firebaseapp.com",
+  databaseURL: "https://etsy-93f9a.firebaseio.com",
+  projectId: "etsy-93f9a",
+  storageBucket: "etsy-93f9a.appspot.com",
+  messagingSenderId: "65577396218"
+  };
